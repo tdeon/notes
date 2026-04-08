771feae63542d08a9b0ae78d7a0fe84b47e0faeb
acovdu <acovdu@163.com>
2026-04-08 23:33:37 +0800

    deploy, %commit_info%

--- deploy.bat
+++ deploy.bat
@@ -5,10 +5,10 @@ chcp 65001
 set username=acovdu
 set useremail=acovdu@163.com
 
-set /p commit_info=输入提交信息：
-
 git config user.name "%username%"
 git config user.email "%useremail%"
+
+set /p commit_info=输入提交信息：
 
 git add -A
 git commit -m "deploy, %commit_info%"
